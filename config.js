141a581710d4a6bb391e4d12c26695c986bd384a
Keng Son <cheongks2@gmail.com>
2021-08-20 23:10:54 +0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     secretKey: "12345-67890-09876-54321",
     mongoUrl: "mongodb://localhost:27017/conFusion",
+    facebook: {
+        clientId: "217544743544815",
+        clientSecret: "5bf8b67c81f5f044f8e1e0183ebc0eee",
+    },
 };
